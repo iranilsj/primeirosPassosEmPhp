f2448118c4df80541258ce7fbc55d21b60fbada1
Iran <irani.fpo@gmail.com>
2024-06-23 17:13:53 -0300

    operacoesMatematicas

---
+++ operacoesMatematicas.php
@@ -0,0 +1,44 @@
+<?php
+
+// ADIÇÃO 
+// 1 ) Forma
+$soma = 20 + 10;
+echo $soma;
+// 2) Forma
+echo "\n"; // PULAR LINHA
+$soma1 = 20;
+$valor = $soma1 + 20;
+echo $valor;
+
+//SUBTRAÇÃO 
+//1) Forma
+echo "\n";
+$sub = 20 - 10;
+echo $sub;
+//2) Forma
+echo "\n";
+$sub2 = 20;
+$valor1 = $sub2 - 10;
+echo $valor1;
+
+//MULTIPLICAÇÃO
+//1) Forma
+echo "\n";
+$mult = 20 * 20;
+echo $mult;
+//2) Forma
+echo "\n";
+$mult1 = 20;
+$valor3 = $mult1 * 20;
+echo $valor3;
+
+//DIVISÃO 
+//1) Forma
+echo "\n";
+$div = 20 / 20;
+echo $div;
+//2) Forma
+echo "\n";
+$div1 = 20;
+$valor4 = $div1 / 20;
+echo $valor4;
